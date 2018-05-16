989a50d1e5d2e5c8247c54283129e901410ff733
melissa.e.cruz <melissa.e.cruz@gmail.com>
2018-05-16 18:51:01 -0400

    added morgan

--- webnotes.js
+++ webnotes.js
@@ -1,3 +1,6 @@
+const morgan = require('morgan');
+
+
 const notes = [
   'http is a protocol',
   'http requests have a url, method, header, and body',
@@ -6,21 +9,40 @@ const notes = [
 
 const express = require('express')
 const app = express()
-
+app.use(morgan('tiny'));
 //app.get('/', (req, res) => res.send('Web Notes'))
 //app.use('/', express.static('views'));
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
+
 app.set('view engine', 'ejs');
+app.use('/css', express.static('css'));
 
 app.get('/',(req,res) =>{
   res.render('notes',{notes:notes});
 });
-app.use('/css', express.static('css'));
+
 
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true }));
+
 app.post('/notes', (req, res) => { 
   notes.push(req.body.note);
   res.redirect('/');
 });
+
+
+//curl command
+app.delete('/notes/:notesID', (req,res) =>{
+  
+if(req.params.notesID <notes.length){
+  notes.splice(req.params.notesID,1);
+  res.send('deleted fancy');
+  }
+  
+else{
+res.status(404).send('not found');
+  }
+  
+});
+
